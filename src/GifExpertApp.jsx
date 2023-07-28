fcd31f9a7596368ece97011a4d9f4917645c53b4
alexander98in <alexander98in@gmail.com>
2023-07-27 23:17:35 -0500

    Creación del componente AddCategory

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,8 +1,28 @@
+import { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
-  return (
-    <>
-        <h1>Gif Expert App</h1>
-    </>
-  )
+
+    const [categories, setCategories] = useState(['One Punch']);
+
+    const onAddCategory = () => {
+        setCategories([ 'Valorant', ...categories ]);
+        //setCategories( cat => [...cat, 'Valorant']);
+    }
+    return (
+        <>
+            <h1>Gif Expert App</h1>
+
+            {/* Input */}
+            <AddCategory/>
+            <button onClick={ onAddCategory }>Agregar</button>
+            <ol>
+                {
+                    categories.map( category => {
+                        return <li key={ category }>{ category }</li>
+                    })
+                }
+            </ol>
+        </>
+    )
 }
